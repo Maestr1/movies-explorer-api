4fb4d665496599da7d14ef741d25b75f8fbbd726
Maestr1 <maestr234@gmail.com>
2023-01-27 00:36:21 +0300

    feat: созданы модели данных для БД

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Сервер запущен на порту ${PORT}`);
---
+++ models/movies.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const { isURL } = require('validator');
+
+const cardSchema = mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => isURL(url),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => isURL(url),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => isURL(url),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  // likes: [{
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   default: [],
+  //   ref: 'user',
+  // }],
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
+}, { versionKey: false });
+
+module.exports = mongoose.model('card', cardSchema);
