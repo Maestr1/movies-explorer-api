32653aacebdc834c8a48a429593193e519dbd1d5
Maestr1 <maestr234@gmail.com>
2023-03-03 01:17:37 +0300

    fix: исправлены ожидаемые аргументы при редактировании user, изменены тексты ошибок

--- models/user.js
+++ models/user.js
@@ -30,12 +30,12 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(email,
     .select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new Error('Вы ввели неправильную почту или пароль.'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new Error('Вы ввели неправильную почту или пароль.'));
           }
           return user;
         });
